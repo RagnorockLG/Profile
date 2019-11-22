304812d55aba4390236ec57260f539c1e195fe03
John Mc Gowan <johnliammcgowan@gmail.com>
2019-11-22 18:14:17 +0000

    Initial Layout

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/*JavaScript*/
+
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("nav").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("nav").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
